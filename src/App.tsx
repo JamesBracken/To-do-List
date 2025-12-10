ca013684d31685215235ef27fa76e24765660ba6
JamesBracken <james_bracken@yahoo.com>
2025-12-10 20:20:42 +0000

    Fix: replace faulty cognito login link

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import ToDoList from './components/toDoList/ToDoList';
 import AddTask from './components/addTask.tsx/AddTask';
 import { logIn, loginTest } from './login';
 import config from "./config.json";
-
+import { generateCodeVerifier, generateCodeChallenge } from './PKCEHelper';
 function App() {
-
+  console.log("generateCodeVerifier:", generateCodeVerifier())
   return (
     <>
       <button id='counter' onClick={() => {
--- src/components/navBar/Nav.tsx
+++ src/components/navBar/Nav.tsx
@@ -2,7 +2,11 @@ const Nav = () => {
     return (
         <>
             <nav>
-                <a href="https://eu-north-1dsoci5dtk.auth.eu-north-1.amazoncognito.com/login?client_id=o817ick1fj45frs5gg42nv0sq&redirect_uri=https://d84l1y8p4kdic.cloudfront.net&response_type=code&scope=email+openid+phone">Login</a>
+                <button onClick={
+                    () => {
+                        window.location.href="https://eu-north-1dsoci5dtk.auth.eu-north-1.amazoncognito.com/login/continue?client_id=o817ick1fj45frs5gg42nv0sq&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2F&response_type=code&scope=email+openid+profile"
+                    }
+                }>Cognito login btn</button>
                 <a href="">Signup</a>
             </nav>
         </>
