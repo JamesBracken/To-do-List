1e0510979efc2a90b9cbbf3b781704a92e574b8f
JamesBracken <james_bracken@yahoo.com>
2025-12-23 18:58:39 +0000

    Refactor: authContextClear and handleLogout into useHandleLogout

--- src/hooks/authHooks.tsx
+++ src/hooks/authHooks.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../components/authContext/AuthContext";
+import { COGNITO_BASE_URL } from "../constants";
+import config from "../config.json";
 
 export const useAuth = () => {
     const context = useContext(AuthContext)
@@ -8,10 +10,17 @@ export const useAuth = () => {
     return { tokens, setTokens, user, isAuthenticated };
 }
 
-export const useClearAuthContext = () => {
+export const useHandleLogout = () => {
     const context = useContext(AuthContext)
-    if (context) {
-        const { setTokens } = context;
-        setTokens(null);
+    const codeVerifier = sessionStorage.getItem("codeVerifier");
+    return () => {
+        //Ensure clean auth state before login redirection
+        if (context) {
+            const { setTokens } = context;
+            setTokens(null);
+        }
+        if (codeVerifier) sessionStorage.removeItem("codeVerifier")
+        window.location.href = `${COGNITO_BASE_URL}logout?client_id=${config.amplify.userPoolClientId}&logout_uri=http%3A%2F%2Flocalhost%3A5173%2F`
     }
+
 }
